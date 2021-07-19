42d5ca5e1a4aca2426dc816a2a82fb5ecfa15c46
sugmin <sugmin@LAPTOP-GMO6265F>
2021-07-19 16:50:14 +0900

    차석민

---
+++ jcappy/src/main/java/jcappy/admin/item/ItemDao.java
@@ -0,0 +1,18 @@
+package jcappy.admin.item;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ItemDao {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	public List<ItemVo> selectAll(ItemVo vo) {
+		return sqlSession.selectList("item.selectAll", vo);
+	}
+}
---
+++ jcappy/src/main/java/jcappy/admin/item/ItemServiceImpl.java
@@ -0,0 +1,18 @@
+package jcappy.admin.item;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemServiceImpl implements ItemService{
+
+	@Autowired
+	ItemDao dao;
+	
+	@Override
+	public List<ItemVo> selectAll(ItemVo vo) {
+		return dao.selectAll(vo);
+	}
+}
