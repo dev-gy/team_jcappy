0b87bf8ba358463d171688b26f5a2402c2396200
seee0 <seee0@DESKTOP-I4RPV02>
2021-07-21 14:59:49 +0900

    회원 작업

---
+++ jcappy/src/main/java/jcappy/members/MembersDao.java
@@ -0,0 +1,18 @@
+package jcappy.members;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MembersDao {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	public MembersVo signin(MembersVo vo) {
+		return sqlSession.selectOne("members.signin", vo);
+	}
+	
+
+}
