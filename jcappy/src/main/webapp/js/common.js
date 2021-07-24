c2c3ad94897d981318b4b53318720d9df50e63b7
ummnaa <ummnaa@211.250.112.138>
2021-07-25 00:49:31 +0900

    에디터 입력창 크기조절바 재활성화

--- jcappy/src/main/webapp/js/common.js
+++ jcappy/src/main/webapp/js/common.js
@@ -9,8 +9,8 @@ function setEditor(holder) {
 		sSkinURI: "/jcappy/smarteditor/SmartEditor2Skin.html",
 		htParams: {
 			bUseToolbar: true,            // 툴바 사용 여부 (true:사용/ false:사용하지 않음)
-			bUseVerticalResizer: false,      // 입력창 크기 조절바 사용 여부 (true:사용/ false:사용하지 않음)
-			bUseModeChanger: false,         // 모드 탭(Editor | HTML | TEXT) 사용 여부 (true:사용/ false:사용하지 않음)
+			bUseVerticalResizer: true,      // 입력창 크기 조절바 사용 여부 (true:사용/ false:사용하지 않음)
+			bUseModeChanger: true,         // 모드 탭(Editor | HTML | TEXT) 사용 여부 (true:사용/ false:사용하지 않음)
 			fOnBeforeUnload: function() {
 				//alert("아싸!");   
 			}
