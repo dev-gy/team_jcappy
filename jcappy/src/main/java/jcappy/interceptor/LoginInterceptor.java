8134694bbec7af4a1c6dd891121f26e1b250e1c2
master <styles0916@naver.com>
2021-08-09 15:16:50 +0900

    @RequestMapping("/pay") // 인터셉터 걸기위해 PostMapping을 RequestMapping으로 바꿈.

--- jcappy/src/main/java/jcappy/interceptor/LoginInterceptor.java
+++ jcappy/src/main/java/jcappy/interceptor/LoginInterceptor.java
@@ -1,22 +1,34 @@
 package jcappy.interceptor;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import jcappy.members.MembersVo;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
 
-		if (request.getSession().getAttribute("membersInfo") != null) {
+		HttpSession session = request.getSession();
+		MembersVo vo = (MembersVo) session.getAttribute("membersInfo");
+		if (vo != null) {
 			return true;
 		} else {
 			response.setContentType("text/html; charset=UTF-8");
-			response.sendRedirect("/jcappy/login");
+			PrintWriter out = response.getWriter();
+			out.print("<script>");
+			out.print("alert('로그인 후 이용가능합니다.');");
+			out.print("location.href='/jcappy/login';");
+			out.print("</script>");
 			return false;
 		}
+
 	}
 }
